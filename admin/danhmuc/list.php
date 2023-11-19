b5c6c1f67f3444d8bc596026c72147a132dc6520
dodohung234 <qqnsbzman@gmail.com>
2023-11-19 09:09:58 +0700

    css lan1

--- admin/danhmuc/list.php
+++ admin/danhmuc/list.php
@@ -19,7 +19,7 @@
                                 $xoadm="index.php?act=xoadm&id=".$id;
                                 
                                 echo '<tr>
-                                <td><input type="checkbox" name="" id=""></td>
+                              
                                 <td>'.$id.'</td>
                                 <td>'.$name.'</td>
                                 <td><a href="'.$suadm.'"><input type="button" value="Sửa"></a> <a href="'.$xoadm.'"><input type="button" value="Xóa"></a></td>
