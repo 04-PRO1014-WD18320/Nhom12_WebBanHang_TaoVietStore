413ce20a70cb1331514b63831b26de9ac98444cf
dodohung234 <qqnsbzman@gmail.com>
2023-11-23 13:22:21 +0700

    xong lan 1 logicsanphamct

--- view/sanphamct.php
+++ view/sanphamct.php
@@ -1,16 +1,36 @@
-<?php 
-                  extract($onesp);
+                <?php
+                extract($onesp);
                 ?>
-                    <div class="boxtitle">
-                    <?=$name?>
-                    </div>
-                    <div class="row boxcontent">
-                        <?php
-                          
-                            $img=$img_path.$img;
-                            echo '<div class="row mb spct"><img src="'.$img.'"></div>';
-                            echo $mota;
+                <div class="boxtitle">
+                    <?= $name ?>
+                </div>
+                <div class="ctai1">
+                    <?php
 
-                        ?>
-                    </div>
+                    $img = $img_path . $img;
+                    echo '<div class="row mb spct"><img src="' . $img . '"></div>';
+                    echo $price;
+                    echo $mota;
+
+                    ?>
+                </div>
+
+
+                
+                <div class="ftclone1">
+                    Sản phẩm cùng loại
+                </div>
+                <div class="contentt">
+                    <?php
+                    foreach ($sp_cung_loai as $sp_cung_loai) {
+                        extract($sp_cung_loai);
+                        $img = $img_path . $img;
+                        $linksp = "index.php?act=sanphamct&idsp=" . $id;
+                        echo '<div class="row mb spct2"><img src="' . $img . '"></div>';
+                        echo '<li><a href="' . $linksp . '">' . $name . '</a></li>';
+                    }
+
+
+                    ?>
+                </div>
                 </div>
